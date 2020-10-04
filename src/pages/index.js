0c1f70f47b780db7bd1e87044687b2efde616282
Jason Evans <evans.jason@hotmail.co.uk>
2020-10-04 17:35:51 +0100

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,64 @@ import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
 
-export default () => {
-  return <Layout>
-    <Hero></Hero>
+export default ({data}) => {
+  const {allStrapiProjects:{nodes:projects},
+  allStrapiBlogs:{nodes:blogs}
+} = data
+
+  return (
+    <Layout>
+    <Hero />
+    <Services />
+    <Jobs />
+    <Projects projects={projects} title="featured projects" showLink />
+    <Blogs blogs={blogs} title="latest articles" showLink />
   </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+
+  }
+`
 // ...GatsbyImageSharpFluid
