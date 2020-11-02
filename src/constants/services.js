82f4d5473cc2b2f24a77b5e7ab3f681b1b364d8b
Jason Evans <evans.jason@hotmail.co.uk>
2020-11-02 19:34:05 +0000

    updated

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,23 @@
 import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import { FaCode, FaDatabase, FaServer, } from "react-icons/fa"
+
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
+    title: "Front-end Development",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "web design",
+    icon: <FaDatabase className="service-icon" />,
+    title: "Database",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
+    icon: <FaServer className="service-icon" />,
+    title: "Deployment",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
   },
 ]
